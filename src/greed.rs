beb261698fa0325774f94d1f9f1d7263b21920a9
Ernstjan Hoek <ernstjanhoek@tutanota.com>
2024-05-12 18:01:01 +0200

    greed af

--- src/greed.rs
+++ src/greed.rs
@@ -36,15 +36,6 @@ pub fn score(dice: [u8; 5]) -> u32 {
 
     score
 }
-// Three 1's => 1000 points
-// Three 6's =>  600 points
-// Three 5's =>  500 points
-// Three 4's =>  400 points
-// Three 3's =>  300 points
-// Three 2's =>  200 points
-// One   1   =>  100 points
-// One   5   =>   50 point
-//noinspection ALL
 #[cfg(test)]
 mod tests {
     use super::score;
